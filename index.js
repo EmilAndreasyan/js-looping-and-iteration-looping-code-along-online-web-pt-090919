56b67f6791980f097ba28a0ba13e3419f93168c0
Emil Andreasyan <emil.andreasyan@yahoo.com>
2020-03-22 05:45:32 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+cards = (["Ada", "Brendan", "Ali"], "birthday");
+function writeCards(cards) {
+  for (let i = 0; i < cards.length; i++) {
+    console.log(`Thank you, ${cards[[i]]}, for the wonderful ${cards[length - 1]} gift`)
+  }
+  return cards;
+}
+writeCards(cards);
+
+function countDown() {
+  let i = 10;
+  while (i > 0) {
+    console.log(i--);
+  }
+}
